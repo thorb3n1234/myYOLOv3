e02568c92f7e7d5bf54ba277926f30b54869e130
thorb3n1234 <thorben.kompalla@googlemail.com>
2020-06-29 11:59:59 +0200

    init - weights

---
+++ create_train_test_txt.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+a = open("config/train.txt", "w")
+for file in os.listdir("../GIT_DATA/ML_DATA/data/data/train/"):
+	if file.endswith('.jpg'):
+		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+		#a.write("../GIT_DATA/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+
+a.close()
+
+b = open("config/test.txt", "w")
+for file in os.listdir("../GIT_DATA/ML_DATA/data/data/test/"):
+	if file.endswith('.jpg'):
+		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+		#b.write("../GIT_DATA/ML_DATA/data/data/test/"+ str(file) + os.linesep)
+b.close()
+
