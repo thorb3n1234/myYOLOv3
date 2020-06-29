3b9fa25d90bfc76dc0ad07da2625c6a5215fc0f8
thorb3n1234 <56269723+thorb3n1234@users.noreply.github.com>
2020-06-29 12:25:26 +0200

    abc

--- create_train_test_txt.py
+++ create_train_test_txt.py
@@ -13,7 +13,7 @@ a.close()
 b = open("config/test.txt", "w")
 for file in os.listdir("../daten/ML_DATA/data/data/test/"):
 	if file.endswith('.jpg'):
-		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+		b.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
 		#b.write("../GIT_DATA/ML_DATA/data/data/test/"+ str(file) + os.linesep)
 b.close()
 
